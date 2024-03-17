1a1b410e01f0192e18f9e3a9d8e6ad027a78cfcd
jhill1 <jon.hill@york.ac.uk>
2024-03-17 11:13:49 +0000

    Adding boilerplate code

---
+++ tidal_analysis.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+# import the modules you need here
+import argparse
+
+def read_tidal_data(filename):
+
+    return 0
+    
+def extract_single_year_remove_mean(year, data):
+   
+
+    return 
+
+
+def extract_section_remove_mean(start, end, data):
+
+
+    return 
+
+
+def join_data(data1, data2):
+
+    return 
+
+
+
+def sea_level_rise(data):
+
+                                                     
+    return 
+
+def tidal_analysis(data, constituents, start_datetime):
+
+
+    return 
+
+def get_longest_contiguous_data(data):
+
+
+    return 
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+                     prog="UK Tidal analysis",
+                     description="Calculate tidal constiuents and RSL from tide gauge data",
+                     epilog="Copyright 2024, Jon Hill"
+                     )
+
+    parser.add_argument("directory",
+                    help="the directory containing txt files with data")
+    parser.add_argument('-v', '--verbose',
+                    action='store_true',
+                    default=False,
+                    help="Print progress")
+
+    args = parser.parse_args()
+    dirname = args.directory
+    verbose = args.verbose
+    
+
+
